8ad156d36c050ed1aa2ca0d0961608054cf7a392
Islan <islan12victhor@gmail.com>
2024-07-07 09:35:21 -0300

    Alguns ajustes no CSS para melhorar a responsabilidade e adicionar efeitos :)

--- static/index.js
+++ static/index.js
@@ -5,7 +5,7 @@ $.getJSON('static/data.json', function (data) {
     habilidadesUL.empty()
     $.each(data.habilidades, function (index, habilidade) {
         habilidadesUL.append(`
-            <li><img src="static/images/icons/${habilidade}" class="w-12"></img></li>
+            <li><img src="static/images/icons/${habilidade}_256.webp" class="w-12 hover:w-16"></img></li>
         `)
     })
 
@@ -13,8 +13,19 @@ $.getJSON('static/data.json', function (data) {
     $.each(data.projetos, function (index, projeto) {
         projetosUL.append(`
             <li><div>
-                <p class="text-blue-600"><a href="${projeto.url}" target="_blank">${projeto.nome}</a></p> <p>${projeto.descricao}</p>
+                <p class="text-blue-600"><a href="${projeto.url}" target="_blank">${projeto.nome}</a></p>
+                <p>${projeto.descricao}</p>
+                <ul id="projeto-${index}-habilidades" class="flex justify-center items-center space-x-1  my-2 min-h-24">
+                </ul>
             </div></li>
         `)
+
+
+        $.each(projeto.habilidades, function (i, habilidade) {
+
+            $('#projeto-' + index + '-habilidades').append(`
+                <li><img src="static/images/icons/${habilidade}_256.webp" class="w-8 hover:w-10"></img></li>    
+            `)
+        })
     })
 })
