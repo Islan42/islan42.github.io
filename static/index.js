e7ae4e3be79b3ac4458a083a6b7624c621e4b99c
Islan <islan12victhor@gmail.com>
2024-07-07 12:16:16 -0300

    Adicionei filtragem por Habilidades

--- static/index.js
+++ static/index.js
@@ -1,17 +1,29 @@
 const habilidadesUL = $('#habilidadesList')
 const projetosUL = $('#projetosList')
 
+let toggle = ''
+
 $.getJSON('static/data.json', function (data) {
     habilidadesUL.empty()
     $.each(data.habilidades, function (index, habilidade) {
-        habilidadesUL.append(`
-            <li class="my-2"><img src="static/images/icons/${habilidade}_256.webp" class="w-12 hover:w-16"></img></li>
+        const habilidadeObjeto = $(`
+            <li><img src="static/images/icons/${habilidade}_256.webp" class="w-12 hover:w-16"></img></li>
         `)
+        
+        habilidadesUL.append(habilidadeObjeto)
+        
+        habilidadeObjeto.on('click', function (event) {
+            if (toggle !== habilidade) {
+                filtrarPorHabilidade(habilidade)
+            } else {
+                filtrarPorHabilidade(habilidade, 'invertido')
+            }
+        })
     })
 
     projetosUL.empty()
     $.each(data.projetos, function (index, projeto) {
-        projetosUL.append(`
+        const projetoObjeto = $(`
             <li><div>
                 <p class="text-blue-600"><a href="${projeto.url}" target="_blank">${projeto.nome}</a></p>
                 <p>${projeto.descricao}</p>
@@ -19,13 +31,28 @@ $.getJSON('static/data.json', function (data) {
                 </ul>
             </div></li>
         `)
-
+        projetosUL.append(projetoObjeto)
+        projetoObjeto.addClass(projeto.habilidades)
 
         $.each(projeto.habilidades, function (i, habilidade) {
-
             $('#projeto-' + index + '-habilidades').append(`
                 <li><img src="static/images/icons/${habilidade}_256.webp" class="w-8 hover:w-10"></img></li>    
             `)
         })
     })
 })
+
+function filtrarPorHabilidade(habilidade, sentido = '') {
+    $.each(projetosUL.children().toArray(), function (i, projeto) {
+        $(projeto).removeClass('hidden')
+        
+        if (sentido !== 'invertido') {
+            if (! $(projeto).hasClass(habilidade)) {
+                $(projeto).addClass('hidden')
+            }
+            toggle = habilidade
+        } else {
+            toggle = ''
+        }
+    })
+}
